8f3171e26c7b13022a9a67add149ef1dd5425656
Thiago Santos <thiago@camposdevelopers.com.br>
2021-11-25 17:58:31 -0300

    feat: create tests in application

--- src/infra/repo/client_repository.py
+++ src/infra/repo/client_repository.py
@@ -2,15 +2,16 @@ from collections import namedtuple
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Clients
 
+
 class ClientRepository:
-    """ class to manager Clients Repository  """
+    """class to manager Clients Repository"""
 
     @classmethod
-    def insert_client(cls, type_person : str, name_person : str):
-        """ insert data in user entity
+    def insert_client(cls, type_person: str, name_person: str):
+        """insert data in user entity
         :param - type_person: type of person Física or Jurídica
         :param - name_person: name of person
-        :return - None  
+        :return - tuple with new client inserted
         """
 
         insert_data = namedtuple("clients", "id, type, name")
@@ -21,7 +22,9 @@ class ClientRepository:
                 db_connection.session.add(new_client)
                 db_connection.session.commit()
 
-                return insert_data(id=new_client.id, type=new_client.type, name=new_client.name)
+                return insert_data(
+                    id=new_client.id, type=new_client.type, name=new_client.name
+                )
             except:
                 db_connection.session.rollback()
                 raise
--- src/infra/repo/client_repository_test.py
+++ src/infra/repo/client_repository_test.py
@@ -1,15 +1,24 @@
 from faker import Faker
+from src.infra.config import DBConnectionHandler
 from .client_repository import ClientRepository
 
 faker = Faker()
 client_repository = ClientRepository()
+db_connection_handler = DBConnectionHandler()
+
 
 def test_insert_client_repository():
-    """ Test insert in client """
+    """Test insert in client"""
 
     type_person = "fisica"
     name_person = faker.name()
 
-    new_client = client_repository.insert_client(type_person, name_person)
+    engine = db_connection_handler.get_engine()
 
-    print(new_client)
+    new_client = client_repository.insert_client(type_person, name_person)
+    query_client = engine.execute(
+        "SELECT * FROM clients WHERE ID='{}';".format(new_client.id)
+    ).fetchone()
+
+    assert new_client.id == query_client.id
+    assert new_client.name == query_client.name
