7b8015d2ecd658749f5dc52a58c2d7cc28b535dc
Thiago Santos <thiago@camposdevelopers.com.br>
2021-11-25 14:48:33 -0300

    fix: update gitignore

--- src/infra/repo/client_repository.py
+++ src/infra/repo/client_repository.py
@@ -2,4 +2,5 @@ class ClientRepository:
     """ class to manager Clients Repository  """
 
     def insert_client(self, type_person : str, name_person : str):
-        """ insert data in user entity """
+        """ insert data in user entity
+        :param - type_person  """
